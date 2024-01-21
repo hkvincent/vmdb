c2326c79771359eeb4714ec0e3931637fb84bc16
vincent <hkvincentchan@gmail.com>
2024-01-21 12:56:38 +0800

    copy right

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <footer className="text-center pb-3 mt-5">
-            <p className="text-sm text-gray-500">@2023 - Vincent Chan</p>
+            <p className="text-sm text-gray-500"><footer>&copy; Strings {new Date().getFullYear()} - Vincent</footer></p>
         </footer>
     )
 }
