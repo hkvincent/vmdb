de89c47cc833973da70927a255cf714d7d058a50
vincent <hkvincentchan@gmail.com>
2024-01-28 15:20:09 +0800

    fix hydration

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Footer() {
     return (
         <footer className="text-center pb-3 mt-5">
-            <p className="text-sm text-gray-500"><footer>&copy; VMDB {new Date().getFullYear()} - Vincent</footer></p>
+            <p className="text-sm text-gray-500">&copy; VMDB {new Date().getFullYear()} - Vincent</p>
         </footer>
     )
 }
